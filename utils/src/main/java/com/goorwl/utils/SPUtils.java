ec1c2e5f21df082a31fad68c606ee82a5aa11552
goorwl <goorwl@163.com>
2019-03-28 17:40:13 +0800

    update sputils;add threadutils

--- utils/src/main/java/com/goorwl/utils/SPUtils.java
+++ utils/src/main/java/com/goorwl/utils/SPUtils.java
@@ -4,34 +4,89 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class SPUtils {
+    private static SharedPreferences sSharedPreferences;
 
-    public static void putBoolean(Context ctx, String key, boolean value) {
-        SharedPreferences sp = ctx.getSharedPreferences("config", Context.MODE_PRIVATE);
-        sp.edit().putBoolean(key, value).commit();
+    private SPUtils() {
     }
 
-    public static boolean getBoolean(Context ctx, String key, boolean defValue) {
-        SharedPreferences sp = ctx.getSharedPreferences("config", Context.MODE_PRIVATE);
-        return sp.getBoolean(key, defValue);
+    public static void init(Context context) {
+        if (sSharedPreferences == null) {
+            synchronized (SPUtils.class) {
+                if (sSharedPreferences == null) {
+                    sSharedPreferences = context.getSharedPreferences("config", Context.MODE_PRIVATE);
+                }
+            }
+        }
     }
 
-    public static void putString(Context ctx, String key, String value) {
-        SharedPreferences sp = ctx.getSharedPreferences("config", Context.MODE_PRIVATE);
-        sp.edit().putString(key, value).commit();
+    public static void putBoolean(String key, boolean value) {
+        if (sSharedPreferences == null) {
+            return;
+        }
+        sSharedPreferences.edit().putBoolean(key, value).commit();
     }
 
-    public static String getString(Context ctx, String key, String defValue) {
-        SharedPreferences sp = ctx.getSharedPreferences("config", Context.MODE_PRIVATE);
-        return sp.getString(key, defValue);
+    public static boolean getBoolean(String key, boolean defValue) {
+        if (sSharedPreferences == null) {
+            return false;
+        }
+        return sSharedPreferences.getBoolean(key, defValue);
     }
 
-    public static void putInt(Context ctx, String key, int value) {
-        SharedPreferences sp = ctx.getSharedPreferences("config", Context.MODE_PRIVATE);
-        sp.edit().putInt(key, value).commit();
+    public static void putString(String key, String value) {
+        if (sSharedPreferences == null) {
+            return;
+        }
+        sSharedPreferences.edit().putString(key, value).commit();
     }
 
-    public static int getInt(Context ctx, String key, int defValue) {
-        SharedPreferences sp = ctx.getSharedPreferences("config", Context.MODE_PRIVATE);
-        return sp.getInt(key, defValue);
+    public static String getString(String key, String defValue) {
+        if (sSharedPreferences == null) {
+            return "";
+        }
+        return sSharedPreferences.getString(key, defValue);
+    }
+
+    public static void putInt(String key, int value) {
+        if (sSharedPreferences == null) {
+            return;
+        }
+        sSharedPreferences.edit().putInt(key, value).commit();
+    }
+
+    public static int getInt(String key, int defValue) {
+        if (sSharedPreferences == null) {
+            return 0;
+        }
+        return sSharedPreferences.getInt(key, defValue);
+    }
+
+    public static void putFloat(String key, float value) {
+        if (sSharedPreferences == null) {
+            return;
+        }
+        sSharedPreferences.edit().putFloat(key, value).commit();
+    }
+
+    public static Float getFloat(String key, Float defValue) {
+        if (sSharedPreferences == null) {
+            return 0f;
+        }
+        return sSharedPreferences.getFloat(key, defValue);
+    }
+
+
+    public static void putLong(String key, long value) {
+        if (sSharedPreferences == null) {
+            return;
+        }
+        sSharedPreferences.edit().putLong(key, value).commit();
+    }
+
+    public static long getInt(String key, long defValue) {
+        if (sSharedPreferences == null) {
+            return 0l;
+        }
+        return sSharedPreferences.getLong(key, defValue);
     }
 }
